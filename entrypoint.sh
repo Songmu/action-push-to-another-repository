0281cc9ace6380f625e50b62a5697425522e4cd0
Songmu <y.songmu@gmail.com>
2025-06-23 22:12:40 +0900

    remove noisy comment

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,8 +9,6 @@ LAST_COMMIT_MESSAGE=$(git log -1 --pretty=%B --first-parent -- $SOURCE_DIRECTORY
 CLONE_DIR=$(mktemp -d)/$WORKTREE_DIR
 mv ./$WORKTREE_DIR "$CLONE_DIR"
 
-ls -la "$CLONE_DIR"
-
 TEMP_WORKDIR=$(mktemp -d)
 # This mv has been the easier way to be able to remove files that were there
 # but not anymore. Otherwise we had to remove the files from "$CLONE_DIR",
@@ -19,34 +17,21 @@ mv "$CLONE_DIR/.git" "$TEMP_WORKDIR/.git"
 
 # $DESTINATION_DIRECTORY is '' by default
 ABSOLUTE_DESTINATION_DIRECTORY="$CLONE_DIR/$DESTINATION_DIRECTORY/"
-
 echo "[+] Deleting $ABSOLUTE_DESTINATION_DIRECTORY"
 rm -rf "$ABSOLUTE_DESTINATION_DIRECTORY"
-
 echo "[+] Creating (now empty) $ABSOLUTE_DESTINATION_DIRECTORY"
 mkdir -p "$ABSOLUTE_DESTINATION_DIRECTORY"
 
-echo "[+] Listing Current Directory Location"
-ls -al
-
-echo "[+] Listing root Location"
-ls -al /
-
 mv "$TEMP_WORKDIR/.git" "$CLONE_DIR/.git"
-
-echo "[+] List contents of $SOURCE_DIRECTORY"
-ls "$SOURCE_DIRECTORY"
 
 echo "[+] evacuate source .git directory to avoid conflicts when mirroring the root directory"
 mv "$SOURCE_DIRECTORY/.git" "$TEMP_WORKDIR/.git"
 
 echo "[+] Copying contents of source repository folder $SOURCE_DIRECTORY to folder $DESTINATION_DIRECTORY in destination repository"
 cp -ra "$SOURCE_DIRECTORY"/. "$CLONE_DIR/$DESTINATION_DIRECTORY"
+
 cd "$CLONE_DIR"
-
-echo "[+] Files that will be pushed"
-ls -la
-
+echo "[+] Building commit message"
 COMMIT_MESSAGE="${COMMIT_MESSAGE/LAST_COMMIT_MESSAGE/$LAST_COMMIT_MESSAGE}"
 ORIGIN_COMMIT="$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
 COMMIT_MESSAGE="${COMMIT_MESSAGE/ORIGIN_COMMIT/$ORIGIN_COMMIT}"
@@ -57,7 +42,9 @@ echo "[+] Swap github.workspace with the cloned repository"
 mv "$GITHUB_WORKSPACE" "$TEMP_WORKDIR/$WORKTREE_DIR"
 mv "$TEMP_WORKDIR/.git" "$TEMP_WORKDIR/$WORKTREE_DIR/.git"
 mv "$CLONE_DIR" "$GITHUB_WORKSPACE"
+# Keep the original repository in a sub directory as worktree.
 mv "$TEMP_WORKDIR/$WORKTREE_DIR" "$GITHUB_WORKSPACE/$WORKTREE_DIR"
 
 cd "$GITHUB_WORKSPACE"
+# The worktree directory is ignored from version control.
 echo "$WORKTREE_DIR" >> .git/info/exclude
